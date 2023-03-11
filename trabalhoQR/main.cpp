7e077cd8a1516603626ef2fc129befec61d6dbb4
Polpedu <emailparaoedu@gmail.com>
2023-03-11 20:50:22 +0000

    15 fds

--- trabalhoQR/main.cpp
+++ trabalhoQR/main.cpp
@@ -5,128 +5,8 @@
 #include <chrono>
 #include <math.h>
 
-/*
-Each QR code can be seen as a matrix of binary cells, meaning that each cell can be represented as a black or white pixel/square.
-
-In order to send and store QR codes, compressing QR codes is essential to save network traffic and storage space. So, a tech start up - QR Shorter Inc - is developing a method to compress the QR codes. The method encodes the QR code as summary of its characteristics.
-
-The encoding algorithm goes as follows:
-
-save the information about the size of the QR Code, meaning the number of cells per line/column (N). The QR Shorter Inc. works only with squared QR Codes.
-save the amount of black cells per line (lb, line blacks, array of size N)
-save the amount of black cells per column (cb, column blacks, array of size N)
-save the amount of color transitions per line (lt, line color transitions, array of size N)
-save the amount of color transitions per column (ct, column color transitions, array of size N)
-save the amount of black cells per quadrant (qb, quadrant blacks, array of size 4)
-save the amount of black cells per diagonal - main and antidiagonal - (db, diagonal blacks, array of size 2)
-
-
-Encoding Notes:
-Consider the QR Code as a squared matrix of side N. For a matrix cell c positioned in the line line 1 ≤ l ≤ N and a column 1 ≤ c ≤ N, c belongs to:
-
-quadrant 1 if l ≤ floor(N/2) and c > floor(N/2),
-quadrant 2 if l ≤ floor(N/2) and c ≤ floor(N/2),
-quadrant 3 if l > floor(N/2) and c ≤ floor(N/2),
-quadrant 4 if l > floor(N/2) and c > floor(N/2),
-diagonal 1 (main diagonal) if l = c
-diagonal 2 (antidiagonal) if L-l+1 = c
-Figure 2 shows two examples on how the encoding is performed.
-
-
-From that information, it should be possible to reconstruct (decode) the QR code and save some space. The storage needed for that info is 1+4*N+4+2, which is smaller than N*N, for every N > 5.
-
-Example:
-
-From the encoding
-
-N = 4
-lb = 1 1 1 1
-cb = 1 1 1 1
-lt = 1 2 2 1
-ct = 1 2 2 1
-qb = 0 2 0 2
-db = 4 0
-the following QR code can be decoded:
-
-Oh no, a problem!
-The company found out that, in some cases, two (or more) different QR codes generate the same encoding. See the example in Figure 4.
-
-This is a big step back in their business model. They decided that, instead of withdrawing the idea, they would implement a module that evaluates each encoding and says if it decodes into a single QR code or to multiple QR codes. If more than one QR code can be decoded from that encoding, it will be considered an invalid encoding.
-
-Your job is to create this module. Given an encoding, find out how many QR codes can be generated from it. If only one, print the decoded QR code, if more than one, state the encoding as INVALID and tell how many QR codes it can generate.
-Additional, there might be some defects in the encodings, so if you find one of those cases that cannot generate any QR coded state as DEFECT.
-
-Input
-The input starts with one line containing the number of encodings to be assessed.
-
-Then, for each encoding, the first line contains the size of the QR code in lines/columns: 2 ≤ N ≤ 30
-
-After that:
-
-One line containing N numbers (each between 1 and N), corresponding to the number of black cells in each line 1 to N (lb).
-One line containing N numbers (each between 1 and N), corresponding to the number of black cells in each column 1 to N (cb).
-One line containing N numbers (each between 1 and N-1), corresponding to the number of color transitions in each line 1 to N (lt).
-One line containing N numbers (each between 1 and N-1), corresponding to the number of color transitions in each column 1 to N (ct).
-One line containing 4 numbers (each between 1 and floor(N/2+1)^2), corresponding to the number of black cells in each quadrant 1 to 4 (qb).
-One line containing 2 numbers (each between 1 and N), corresponding to the number of black cells in each diagonal 1 to 2 (db).
-
-
-Output
-For each test case, you should print one of the following possible outputs:
-
-Valid case: 1 QR code decoded
-If only one solution can be generated from the encoding, you should print the following phrase:
-
-VALID: 1 QR Code generated.
-
-After that, print the QR Code generated. Print every white cell as a space character ' ' and every black cell as a cardinal '#' character. Print a frame for the QR code using the character '+' for the corners, '-' for the top and bottom borders and '|' for the left and right borders.
-
-Invalid case: more than one QR code decoded
-If more than one QR Code is decoded, you should print the phrase (being k the number of QR Codes generated):
-
-INVALID: K QR Codes generated.
-
-Defect case: 0 QR codes decoded
-If no QR Code can be decoded from the encoding, than print the phrase:
-
-DEFECT: No QR Code generated.
-
-Example
-Example input:
-
-3
-3
-1 0 1
-1 0 1
-1 0 1
-1 0 1
-0 1 0 1
-2 0
-4
-2 2 2 2
-2 2 2 2
-3 3 3 3
-1 1 1 1
-2 2 2 2
-2 2
-4
-2 3 4 3
-2 3 4 3
-1 1 0 1
-1 1 0 1
-4 3 4 1
-2 4
-Example output:
-
-VALID: 1 QR Code generated!
-+---+
-|#  |
-|   |
-|  #|
-+---+
-INVALID: 2 QR Codes generated!
-DEFECT: No QR Code generated!
-*/
+#define DEBUG 0
+#define TIMES 0
 
 using namespace std;
 
@@ -298,39 +178,40 @@ struct QR_Code
             }
         }
 
-        /* make a print comparing both vector<int> cb, vector<int> lb, vector<int> ct, vector<int> lt, int *qb, int *db with qr_code->cb, qr_code->lb, qr_code->ct, qr_code->lt, qr_code->qb, qr_code->db */
-        // just to print
-        for (int i = 0; i < N; i++)
+        if (DEBUG)
         {
-            cout << "cb e this: " << i << endl;
-            cout << "cb: " << cb[i] << " " << this->cb[i] << endl;
-            cout << "lb: " << lb[i] << " " << this->lb[i] << endl;
-            cout << "ct: " << ct[i] << " " << this->ct[i] << endl;
-            cout << "lt: " << lt[i] << " " << this->lt[i] << endl;
+            for (int i = 0; i < N; i++)
+            {
+                cout << "cb e this: " << i << endl;
+                cout << "cb: " << cb[i] << " " << this->cb[i] << endl;
+                cout << "lb: " << lb[i] << " " << this->lb[i] << endl;
+                cout << "ct: " << ct[i] << " " << this->ct[i] << endl;
+                cout << "lt: " << lt[i] << " " << this->lt[i] << endl;
+            }
+
+            cout << "...." << endl;
+
+            for (int i = 0; i < 4; i++)
+            {
+                cout << "qb: " << qb[i] << " " << this->qb[i] << endl;
+            }
+
+            cout << "...." << endl;
+
+            for (int i = 0; i < 2; i++)
+            {
+                cout << "db: " << db[i] << " " << this->db[i] << endl;
+            }
+
+            cout << "gives:" << endl;
+            cout << "cb == this->cb: " << (cb == this->cb) << endl;
+            cout << "lb == this->lb: " << (lb == this->lb) << endl;
+            cout << "ct == this->ct: " << (ct == this->ct) << endl;
+            cout << "lt == this->lt: " << (lt == this->lt) << endl;
+            cout << "equal(qb, qb + 4, this->qb): " << equal(qb, qb + 4, this->qb) << endl;
+            cout << "equal(db, db + 2, this->db): " << equal(db, db + 2, this->db) << endl;
+            cout << "............." << endl;
         }
-
-        cout << "...." << endl;
-
-        for (int i = 0; i < 4; i++)
-        {
-            cout << "qb: " << qb[i] << " " << this->qb[i] << endl;
-        }
-
-        cout << "...." << endl;
-
-        for (int i = 0; i < 2; i++)
-        {
-            cout << "db: " << db[i] << " " << this->db[i] << endl;
-        }
-
-        cout << "gives:" << endl;
-        cout << "cb == this->cb: " << (cb == this->cb) << endl;
-        cout << "lb == this->lb: " << (lb == this->lb) << endl;
-        cout << "ct == this->ct: " << (ct == this->ct) << endl;
-        cout << "lt == this->lt: " << (lt == this->lt) << endl;
-        cout << "equal(qb, qb + 4, this->qb): " << equal(qb, qb + 4, this->qb) << endl;
-        cout << "equal(db, db + 2, this->db): " << equal(db, db + 2, this->db) << endl;
-        cout << "............." << endl;
 
         return cb == this->cb && lb == this->lb && ct == this->ct && lt == this->lt && equal(qb, qb + 4, this->qb) && equal(db, db + 2, this->db);
     }
@@ -461,17 +342,16 @@ int main()
 
         auto finish = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> elapsed = finish - start;
-        // std::cout << "Elapsed time: " << elapsed.count() << " s\n";
+        if (TIMES) std::cout << "Elapsed time: " << elapsed.count() << " s\n";
     }
     return 0;
 }
-
 
 int start_generating_tree(int N, std::vector<int> lb, std::vector<int> cb, std::vector<int> lt, std::vector<int> ct, int *qb, int *db)
 {
 
     // first detect if the settings are valid
-    if (defect_detection(N, lb, cb, lt, ct, qb, db) == true)
+    if (defect_detection(N, lb, cb, lt, ct, qb, db))
     {
         std::cout << "DEFECT: No QR Code generated!" << std::endl;
         return 0;
@@ -498,9 +378,6 @@ int start_generating_tree(int N, std::vector<int> lb, std::vector<int> cb, std::
     // we need to do some pre processing, set the cells that need to be certain values according to the settings lb, cb, lt, ct, qb, db
     QR_Code qr_code_new = pre_processing(qr_code, lb, cb, lt, ct, qb, db);
 
-    // qr_code_new is the qr pre processed.
-    // cout << "pre-processed:" << endl;
-
     /*
         we need to do some pre processing, set the cells that need to be certain values according to the settings lb, cb, lt, ct, qb, db
         also, we need to find the defects (find if the current lb, cb, lt, ct, qb, db are valid without generating the qr code)
@@ -509,8 +386,8 @@ int start_generating_tree(int N, std::vector<int> lb, std::vector<int> cb, std::
     // carefull this function is recursive and will run thousands of times
     // cout << "branch:" << endl;
     loop_through_all_cells(qr_code_new, 0, 0, lb, cb, lt, ct, qb, db);
-    qr_code_new.cells[0][0] = 3;
-    loop_through_all_cells(qr_code_new, 0, 0, lb, cb, lt, ct, qb, db);
+    /* qr_code_new.cells[0][0] = 3;
+    loop_through_all_cells(qr_code_new, 0, 0, lb, cb, lt, ct, qb, db); */
 
     if (qr_codes_generated == 0)
     {
@@ -543,17 +420,14 @@ void loop_through_all_cells(QR_Code qr_code, int l, int c, std::vector<int> lb, 
     {
         if (l == qr_code.N - 1 && c == qr_code.N - 1)
         {
-            printQR_code(qr_code);
-            if (qr_code.check_if_valid(lb, cb, lt, ct, qb, db) == 1)
+            if (qr_code.check_if_valid(lb, cb, lt, ct, qb, db))
             {
-                // cout << "FOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOO" << endl;
                 if (qr_codes_generated == 0)
                 {
                     qr_to_print = qr_code.copy();
                 }
                 qr_codes_generated++;
             }
-            qr_code.reset_configs();
             return;
         }
 
@@ -566,16 +440,12 @@ void loop_through_all_cells(QR_Code qr_code, int l, int c, std::vector<int> lb, 
         {
             c++;
         }
-
-        loop_through_all_cells(qr_code, l, c, lb, cb, lt, ct, qb, db);
-        return;
     }
 
     if (l == qr_code.N - 1 && c == qr_code.N - 1)
     {
-        if (qr_code.check_if_valid(lb, cb, lt, ct, qb, db) == 1)
+        if (qr_code.check_if_valid(lb, cb, lt, ct, qb, db))
         {
-            // cout << "FOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOOFOOOOOO" << endl;
             if (qr_codes_generated == 0)
             {
                 qr_to_print = qr_code.copy();
@@ -669,6 +539,12 @@ QR_Code pre_processing(QR_Code qr_code, std::vector<int> lb, std::vector<int> cb
             qr_code.ct[i] = 0;
         }
     }
+
+    // deduct the cell in each quadrant
+
+
+
+    
     return qr_code;
 }
 
@@ -694,7 +570,7 @@ int sum(std::vector<int> array, int N)
 
 bool defect_detection(int N, std::vector<int> lb, std::vector<int> cb, std::vector<int> lt, std::vector<int> ct, int *qb, int *db)
 {
-    // first check if lb and cb make sense, floor down N / 2
+    // first check if lb and cb make any sense, floor down N / 2
     int max_transitions = floor(N / 2) + 1;
 
     for (int i = 0; i < N; i++)
